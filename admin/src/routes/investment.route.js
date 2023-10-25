ee0a4b4bdb897831e2ad48649afc6e8e7d99a278
euherrera <eu.herrera@gmail.com>
2023-10-25 21:48:45 +0100

    fixed passing value from one route to another..

--- admin/src/routes/investment.route.js
+++ admin/src/routes/investment.route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { investmentService } = require('../services');
 const { InvestmentController } = require('../controllers');
 const investmentController = new InvestmentController(investmentService);
-const routing = require('../middleware/routing')
+
 
 const router = express.Router();
 
-router.get('/investments/:id', routing, investmentController.getInvestments.bind(investmentController));
+router.get('/investments/:id', investmentController.getInvestments.bind(investmentController));
 router.post('/investments/export', investmentController.postInvestments.bind(investmentController));
 module.exports = router;
