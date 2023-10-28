77d618cf905c1019a81d8b424cd7d84a4759a70a
euherrera <eu.herrera@gmail.com>
2023-10-28 09:53:25 +0100

    fixed routing

--- admin/src/services/investment.service.js
+++ admin/src/services/investment.service.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const fs = require("fs"); 
 const output = './output.json';
 
+
 const config = require("config")
 class InvestmentService  {
   
@@ -11,22 +12,33 @@ class InvestmentService  {
     .get(`${config.investmentsServiceUrl}/investments/${req.params.id}`)
     .then((response) => {
       console.log('passed middleware:', req.csv);
-      
+      const data = {}
+      data.csv = req.csv
+      fs.writeFile(output, JSON.stringify(data), (err) => { 
+        if (err) 
+          console.log(err); 
+        else { 
+          console.log("File written successfully\n"); 
+          console.log("The written has the following contents:"); 
+          console.log(fs.readFileSync("output.json", "utf8")); 
+        } 
+      }); 
       return response.data;
     })
     .catch((error) => {
       console.error('error:', error);
       return error.toString();
     });
-    next(req.csv)
    return request
   }
 
   async postInvestments(res, req, id) {
     
-    const data = {}
-    data.csv = req.csv
-    const response = await axios.post(`${config.investmentsServiceUrl}/investments/export`, data)
+    const data = output
+    // data.csv = req.csv
+
+  
+    const response = await axios.post(`${config.investmentsServiceUrl}/investments/export`, JSON.parse(data))
     .then((response) => {
        
         
